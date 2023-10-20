4a659a614f4a7231720aeca63841e5d9731d7d3e
Haroldo Proenca <haroldoproenca@gmail.com>
2023-10-19 23:40:15 -0300

    push

--- dataproject/main.py
+++ dataproject/main.py
@@ -1,10 +1,10 @@
 from pipeline.extract import extract_from_excel
-from pipeline.transform import concat_data_frames
 from pipeline.load import load_excel
+from pipeline.transform import concat_data_frames
 
-if __name__ == "__main__":
-    data_frame_list = extract_from_excel("data/input")
+if __name__ == '__main__':
+    data_frame_list = extract_from_excel('data/input')
     print(type(data_frame_list))
     data_frame = concat_data_frames(data_frame_list)
     print(type(data_frame))
-    load_excel(data_frame,"data/output", "output")
+    load_excel(data_frame, 'data/output', 'output')
